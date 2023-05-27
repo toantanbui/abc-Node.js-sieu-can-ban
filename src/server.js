ef154bb73655a4c31420974fcdd84b450b64bbc2
Bui Tan Toan <tantoanbui1@gmail.com>
2023-05-27 22:56:56 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initAPIRoute from './route/api';
 
 import iniWebRoute from './route/web';
 import path from 'path';
+import cors from 'cors';
 // import connection from './configs/connectDB';
 require('dotenv').config();
 
@@ -11,10 +13,12 @@ const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ origin: true }));
 
 
 configViewEngine(app);
 iniWebRoute(app);
+initAPIRoute(app);
 
 
 // app.get('/', (req, res) => {
